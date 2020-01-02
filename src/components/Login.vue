2c1560f5de4b105a549a470be40b96e842ffae42
chy <chy>
2020-01-02 20:07:26 +0800

    初始化

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+       用户名：<input type="text" v-model="" >
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
