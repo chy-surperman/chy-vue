49e6450807074e606de4679ab6a307c0fac0bfbe
chy <chy>
2020-01-16 14:43:06 +0800

    登陆页面的搭建

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import ElementUI from 'element-ui'
 import router from './router'
 
 // 设置反向代理，前端请求默认发送到http：/localhost：8443/api
@@ -11,6 +12,8 @@ axios.defaults.baseURL = 'http://localhost:8443/api'
 // 全局注册，之后可在其他组件中通过 this.$axios 发送数据
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
